1945b3f84a1034bde603bbac597223f0fe89269b
Igor <99yearago99@gmail.com>
2022-10-03 21:34:36 +0300

    task 5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
+    int year;
+
+    for (year =1904; year<=2096; year++) {
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year);
+        }
+    }
 
     }
 }
